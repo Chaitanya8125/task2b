75980543bb0168ce1d21cd855731ff94e65b3ce4
Abhijith <24kb1a05e6@nbkrist.org>
2026-09-25 12:21:10 +0530

    Create React counter

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
